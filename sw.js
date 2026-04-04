a6b594946e9225813c70ad13e7bad0841b5ab5ce
Amaya0623 <isicontrolacces@gmail.com>
2026-04-04 18:25:13 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 const CACHE_NAME = 'zeus-barber-v2';
 
+// 1. INSTALACIÓN DEL MOTOR Y GUARDADO EN CACHÉ
 self.addEventListener('install', e => {
+  console.log('[Zeus SW] Instalado como App Web PWA 🚀');
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(['/', '/index.html']);
@@ -8,6 +10,7 @@ self.addEventListener('install', e => {
   );
 });
 
+// 2. INTERCEPTOR (Permite abrir la app incluso sin internet)
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => {
